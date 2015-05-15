e26aea41f6436d5bed90320672fc9108c6257caa
Heineiuo <heineiuo@gmail.com>
2015-05-15 17:24:30 +0800

    update event.js

--- src/event.js
+++ src/event.js
@@ -1,22 +1,10 @@
 
 
-/**
- * @param {__purple.eventLib} 
- * @param {__purple.eventLib.type} 触发方式，点击或者滑动
- * @param {__purple.eventLib.target} dom对象
- * @param {__purple.eventLib.target的值} 函数名
- */
-function clearEvents(machine) {
-  for (var i = 0; i < machine.eventArray.length; i++) {
-    var a = machine.eventArray[i];
-    $(a[0]).off(a[1],a[2],a[3])
-  }
+purple.on = function() {
   
-  machine.eventArray = [] // 清空事件集
 }
 
 
+purple.off = function(){
 
-purple.on = function() {
-  
 }
