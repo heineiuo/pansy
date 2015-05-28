aa1346d65c076cf1ef3003e2d9f1edc387b69528
heineiuo <heineiuo@qq.com>
2015-05-28 08:15:10 +0800

    update core and delete event.js

--- tool/tpl.js
+++ tool/tpl.js
@@ -1,4 +1,5 @@
-// 'use strict';
+'use strict';
+
 /**
  * 加载fs模块
  */
@@ -19,60 +20,76 @@ var fs = require('fs');
  */
 
 var code = 'utf8';
-var htmlpath = 'dist/html/main';
-var htmlsuffix = '.html'
-var regarr = walk3(htmlpath)
+var htmlpath = './src/html-min';
+var hash_suffix = '_'+'hfhg1adf'
+var targetFilename = "./dist/smile_oauth2_hfhg1adf/js/oauth2_tpl" + hash_suffix + ".js"
+var regarr = []
 
-console.log(regarr)
-
-
-function walk3(path){   // 深度优先
-  var fileList = []
-  walk2()
-  return fileList
-
-  function walk2 (argument) {
-    var dirList = fs.readdirSync(path);
-    dirList.forEach(function(item){
-      if(fs.statSync(path + '/' + item).isDirectory()){
-        walk2(path + '/' + item);
-      }else{
-
-        if (/.+\.js/.test(item)) {
-          fileList.push(path + '/' + item);
-        };
-
-      }
-    });
-
-  }
-
-
-}
-
-
-
-
-/**
- * @param {script_str} 
- */
-var script_str = '';
-for (var i = 0; i < regarr.length; i++) {
-  script_str += "purple.html('";
-  script_str += regarr[i];
-  script_str += "','";
-  script_str += fs.readFileSync(htmlpath+regarr[i]+htmlsuffix, code);
-  script_str += "');";
-};
+walk2(htmlpath)
+var script_str = concatStr(regarr)
 
 /**
  * 创建文件
  */
 
- fs.open("webapp/index.html","w",0644,function(e,fd){
+fs.open(targetFilename, "w", '0644', function(e,fd) {
+  if(e) throw e;
+  fs.write(fd, script_str, 0, 'utf8', function(e){
     if(e) throw e;
-    fs.write(fd, script_str,0,'utf8',function(e){
-        if(e) throw e;
-        fs.closeSync(fd);
-    })
+    fs.closeSync(fd);
+  })
 });
+
+function walk2 (path) {
+  var dirList = fs.readdirSync(path);
+  dirList.forEach(function(item){
+
+    if(fs.statSync(path + '/' + item).isDirectory()){
+      walk2(path + '/' + item);
+    }else{
+
+      if (/.+\.html/.test(item)) {
+        regarr.push(path + '/' + item);
+      };
+
+    }
+  });
+
+}
+
+function getName (filename) {
+
+  console.log(filename)
+
+// /(?!dist\/html\/)(.*)(?=\.html)/.exec(regarr[i]);
+  return /.*(?=\.html)/.exec(filename.split(htmlpath+'/')[1])[0]
+
+}
+
+function concatStr (regarr) {
+
+  var script_str = '(function (root, factory) {'
+    + '  if (typeof define === "function" && define.amd) {'
+    + '    define(["purple"], factory);'
+    + '  } else {'
+    + '    root.loadtpl = factory();'
+    + '  }'
+    + '}(this, function () {'
+    + '  function loadtpl() {'
+
+  for (var i = 0; i < regarr.length; i++) {
+    script_str += "purple.html('";
+    script_str += getName(regarr[i])
+    script_str += "','";
+    script_str += fs.readFileSync(regarr[i], code);
+    script_str += "');";
+  };
+
+  script_str += '  }'
+  + '  return loadtpl'
+  + '}));'
+
+  console.log(script_str)
+  return script_str
+}
+
