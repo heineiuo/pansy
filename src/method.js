7cfc591a46b57881601cc145dd90ed44586aaf22
heineiuo <heineiuo@gmail.com>
2015-07-07 14:30:45 +0800

    6.0.1

--- src/method.js
+++ src/method.js
@@ -1,3 +1,21 @@
+/**
+ * ºÊ»›
+ */
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function(fun) {
+    var len = this.length
+    if (typeof fun != "function"){
+      throw new TypeError()
+    }
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++) {
+      if (i in this) {
+        fun.call(thisp, this[i], i, this)
+      }
+    }
+  }
+}
+
 function isEmpty(obj) {
   for(var prop in obj) {
     if(obj.hasOwnProperty(prop))
