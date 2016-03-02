d3dcb5e1a91dd2fbec12fc42a75b79a1e29ad0f4
heineiuo <heineiuo@gmail.com>
2016-03-02 17:24:47 +0800

    rename tasks to scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,33 @@
-module.exports = require('./tasks/grunt')
+'use strict'
+
+var _ = require('lodash')
+
+var tasks = [
+  'uglify',
+  'webpack'
+]
+
+var conf = {}
+
+_.map(tasks, function(item, index){
+  conf[item] = {}
+  conf[item] = _.assign(conf[item], require('./scripts/grunt/release/'+item))
+})
+
+
+module.exports = function(grunt){
+
+  conf.pkg = grunt.file.readJSON('package.json')
+
+  grunt.initConfig(conf);
+
+  grunt.registerTask('release', [
+    'webpack:indexJS',
+    'uglify:indexJS'
+  ])
+
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+}
